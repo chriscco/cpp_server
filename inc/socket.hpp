d3a05abf2acf94c0766670afbabddda06457f416
chriscco <2444628489c@gmail.com>
2025-02-15 23:17:27 -0800

    TODO: fix BUGs

---
+++ inc/socket.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "inetaddr.hpp"
+
+
+class Socket {
+private:
+    int _fd;
+public:
+    Socket();
+    Socket(int);
+    ~Socket();
+
+    void bind(InetAddr*);
+    int accept(InetAddr*);
+
+    void listen();
+    void setnonblocking() ;
+
+    int getfd();
+};
+
+Socket::Socket() : _fd(-1) {
+    _fd = socket(AF_INET, SOCK_STREAM, 0);
+    errif(_fd < 0, "socket error in Socket().");
+}
+
+Socket::Socket(int fd) : _fd(fd) {
+    errif(fd < 0, "socket error in Socket(int).");
+}
+
+void Socket::bind(InetAddr* addr) {
+    int b = ::bind(_fd, (sockaddr*)&addr->get_addr(), addr->get_len());
+    errif(b < 0, "bind error.");
+}
+
+int Socket::accept(InetAddr* addr) {
+    int a = ::accept(_fd, (sockaddr*)&addr->get_addr(), &addr->get_len());
+    errif(a < 0, "accept error.");
+    return a;
+}
+
+void Socket::listen() {
+    errif(::listen(_fd, SOMAXCONN) < 0, "listen error.");
+}
+
+void Socket::setnonblocking() {
+    fcntl(_fd, F_SETFL, fcntl(_fd, F_GETFL) | O_NONBLOCK);
+}
+
+int Socket::getfd() {
+    return _fd;
+}
+
