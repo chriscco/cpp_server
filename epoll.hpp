45ed9e8a35f8b2dd61436f220fa158d37f2c0d84
chriscco <2444628489c@gmail.com>
2025-02-15 22:46:40 -0800

    changed private member naming

--- epoll.hpp
+++ epoll.hpp
@@ -9,8 +9,8 @@
 
 class Epoll {
 private:
-    int epoll_fd;
-    struct epoll_event* events;
+    int _epoll_fd;
+    struct epoll_event* _events;
 public:
     Epoll() {};
     ~Epoll() {};
@@ -20,11 +20,11 @@ public:
     std::vector<epoll_event> add_event(int timeout);
 };
 
-Epoll::Epoll() : epoll_fd(-1), events(nullptr) {
-    epoll_fd = epoll_create1(0);
-    errif(epoll_fd < 0, "epoll_fd creation error.");
-    events = new epoll_event[MAX_EVENTS_SIZE];
-    bzero(events, sizeof(*events) * MAX_EVENTS_SIZE);
+Epoll::Epoll() : _epoll_fd(-1), _events(nullptr) {
+    _epoll_fd = epoll_create1(0);
+    errif(_epoll_fd < 0, "epoll_fd creation error.");
+    _events = new epoll_event[MAX_EVENTS_SIZE];
+    bzero(_events, sizeof(*_events) * MAX_EVENTS_SIZE);
 }
 
 /**
@@ -36,15 +36,15 @@ void Epoll::add_fd(int fd, uint32_t op) {
     bzero(&evt, sizeof(evt));
     evt.data.fd = fd;
     evt.events = op;
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &evt);
+    epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, fd, &evt);
 }
 
 std::vector<epoll_event> Epoll::add_event(int timeout) {
     std::vector<epoll_event> eventsRetrieved;
-    int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS_SIZE, timeout);
+    int nfds = epoll_wait(_epoll_fd, _events, MAX_EVENTS_SIZE, timeout);
     errif(nfds, "epoll wait error.");
     for (int i = 0; i < nfds; ++i) {
-        eventsRetrieved.emplace_back(events[i]);
+        eventsRetrieved.emplace_back(_events[i]);
     }
     return eventsRetrieved;
 }
