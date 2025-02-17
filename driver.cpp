b8ff072b36f5a247155690fb61be1c469a9a6146
chriscco <2444628489c@gmail.com>
2025-02-16 17:08:06 -0800

    renamed

---
+++ driver.cpp
@@ -0,0 +1,26 @@
+#include <sys/socket.h> 
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "inc/epoll.h"
+#include "inc/error_handler.h"
+#include "inc/inetaddr.h"
+#include "inc/socket.h"
+#include "inc/server.h"
+
+#define MAX_EVENT 1024
+
+/** 
+ * 当前代码使用了ET边缘触发
+ */
+int main() {
+    EventLoop* loop = new EventLoop();
+    Server* server = new Server(loop);
+    loop->loop();
+    return 0;
+}
